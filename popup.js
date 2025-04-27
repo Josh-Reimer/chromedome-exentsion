a5645c64a5680eb38910a160a5a97300f1763437
Josh-Reimer <reimer.josh.2006@gmail.com>
2025-04-27 15:11:35 -0300

    added tab counter

--- popup.js
+++ popup.js
@@ -1,10 +1,12 @@
 let output = document.getElementById("output-p");
+let no_of_tabs_h1 = document.getElementById("no-of-tabs");
 let tabs_list = [];
+
 chrome.tabs.query({windowId: chrome.windows.WINDOW_ID_CURRENT}, (tabs) => {
    for(let i = 0; i < tabs.length; i++){
-        output.innerText += tabs[i].url;
+        output.innerText += tabs[i].url+"\n";
+        tabs_list.push(tabs[i].url)
    }
-
+no_of_tabs_h1.innerText = tabs_list.length;
 })
-
 
