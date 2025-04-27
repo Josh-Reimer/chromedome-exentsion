f1660fa6fcdead837a5eeafcac01f3bc85da2c2a
Josh-Reimer <reimer.josh.2006@gmail.com>
2025-04-27 14:57:39 -0300

    added basic tab url collection functionality

--- popup.js
+++ popup.js
@@ -1,1 +1,10 @@
 let output = document.getElementById("output-p");
+let tabs_list = [];
+chrome.tabs.query({windowId: chrome.windows.WINDOW_ID_CURRENT}, (tabs) => {
+   for(let i = 0; i < tabs.length; i++){
+        output.innerText += tabs[i].url;
+   }
+
+})
+
+
